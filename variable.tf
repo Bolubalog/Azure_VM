b06612df09488d02c37ccf24f91055818369e42f
Bolubalog <bolubalog@gmail.com>
2024-05-02 07:05:16 -0400

    Initial Change

--- variable.tf
+++ variable.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7cf986e8c6529458d4c6eea39caff4af42b4bff5d686e690af8b5370ce982c45
-size 334
+oid sha256:a23fbfe76908eb58c06fffeccd30c4669fc00ae16814048175aa268037690574
+size 318
