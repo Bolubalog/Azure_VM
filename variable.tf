86cf4ab2e72518b1b4babde94560424932015800
Bolubalog <bolubalog@gmail.com>
2024-05-02 06:02:33 -0400

    My first Azure_VM

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "subscription_id" {
+  default = "2b2bc4bf-7b77-4309-8730-44c3ffe6f2aa"
+}
+variable "client_id" {
+  default = "537f8d9a-e2e0-4c02-8479-14fe2a02a038"
+}
+variable "client_secret" {
+  default = "qFC8Q~2PWxtTXLeTmUcn7UQwglIWM62r_OgGscXH"
+}
+variable "tenant_id" {
+  default = "ca847a76-c0e7-40d2-a59e-86e650e4ccd1"
+}
+
+
