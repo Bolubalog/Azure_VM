2a372187334f089b53345e50ace7aebced100a46
Bolubalog <bolubalog@gmail.com>
2024-05-02 06:55:52 -0400

    Initial Push

--- variable.tf
+++ variable.tf
@@ -1,14 +1,3 @@
-variable "subscription_id" {
-  default = "2b2bc4bf-7b77-4309-8730-44c3ffe6f2aa"
-}
-variable "client_id" {
-  default = "537f8d9a-e2e0-4c02-8479-14fe2a02a038"
-}
-variable "client_secret" {
-  default = "qFC8Q~2PWxtTXLeTmUcn7UQwglIWM62r_OgGscXH"
-}
-variable "tenant_id" {
-  default = "ca847a76-c0e7-40d2-a59e-86e650e4ccd1"
-}
-
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:7cf986e8c6529458d4c6eea39caff4af42b4bff5d686e690af8b5370ce982c45
+size 334
